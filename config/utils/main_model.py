c4a703bd26d37c3e7417f95d7d5a894a6f6e8265
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-07 11:40:39 -0400

    [FIX] use ActiveManeger in MainModel

--- config/utils/main_model.py
+++ config/utils/main_model.py
@@ -2,31 +2,37 @@
 
 # Django
 from django.db import models
-from django.db.models.functions import Now
+from django.utils.timezone import now
 
-# Models
+# Utilities
 from .base_model import BaseModel
+from apps.core.managers import ActiveManager  # Importamos el nuevo manager
 
 
 class MainModel(BaseModel):
-    """MainModel
+    """Modelo base para incluir `active` y `deleted`."""
 
-    Args:
-        BaseModel (_type_): _description_
-    """
-
-    deleted = models.DateTimeField(verbose_name="deleted", default=None, null=True, blank=True)
+    deleted = models.DateTimeField(
+        verbose_name="deleted", default=None, null=True, blank=True
+    )
 
     active = models.BooleanField(verbose_name="Activo", default=True, help_text="")
 
-    def onDelete(self):
-        self.deleted = Now()
+    objects = ActiveManager()  # 🔥 Filtra por defecto solo activos
+
+    def soft_delete(self):
+        """Elimina el objeto sin borrarlo de la BD (Soft Delete)."""
+        self.deleted = now()
         self.active = False
+        self.save()
 
     def restore(self):
+        """Restaura un objeto eliminado."""
         self.deleted = None
         self.active = True
+        self.save()
 
     class Meta:
         """Meta options."""
+
         abstract = True
