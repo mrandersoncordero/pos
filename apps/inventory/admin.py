934cab37e07f0876e3d58c2ad6c0aceca9680a49
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-07 14:14:53 -0400

    [FIX] admin inventory

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -16,7 +16,7 @@ class CategoryAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Categoria', {
             "fields": (
-                'name', 'description'
+                'name', 'description', 'active'
             ),
         }),
         ('Metadata', {
@@ -28,6 +28,75 @@ class CategoryAdmin(admin.ModelAdmin):
     
     readonly_fields = ('created', 'modified')
 
-admin.site.register(Unit)
-admin.site.register(Store)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'unit', 'store', 'stock', 'price_sale']
+    list_display_links = ['name']
+    list_filter = ['created', 'category__name']
+    search_fields = ['name', 'category__name']
+    ordering = ['created']
+
+    fieldsets = (
+        ('Informacion', {
+            "fields": (
+                'name', 'description',
+                ('category', 'unit', 'store', 'active'),
+                ('stock', 'purchase_price', 'price_sale'),
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            )
+        })
+    )
+    
+    readonly_fields = ('created', 'modified')
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'quantity', 'created']
+    list_display_links = ['id']
+    list_editable = ['name', 'quantity']
+    list_filter = ['name', 'created']
+    search_fields = ['name', 'created']
+    
+    fieldsets = (
+        ('Informacion', {
+            "fields": (
+                'name', 'description', 'quantity'     
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            )
+        })
+    )
+    
+    readonly_fields = ('created', 'modified')
+    
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'address', 'created']
+    list_display_links = ['id']
+    list_editable = ['name']
+    list_filter = ['name', 'created']
+    search_fields = ['id', 'name']
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                'name', 'address'
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            )
+        })
+    )
+    
+    readonly_fields = ('created', 'modified')
+
